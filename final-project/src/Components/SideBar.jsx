bd7bfd5bf22ef6fcf09c49c19a70e1c9ccb41d36
Vexy-04 <varunal2003@gmail.com>
2023-10-24 15:05:11 +0530

    Sidebar

--- final-project/src/Components/SideBar.jsx
+++ final-project/src/Components/SideBar.jsx
@@ -28,12 +28,10 @@ function SearchResult() {
                     title="Customer Review"
                     menuVariant="dark"
                 >
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">
-                        Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.1">⭐⭐⭐⭐</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">⭐⭐⭐</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">⭐⭐</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.1">⭐</NavDropdown.Item>
                 </NavDropdown>
 
                 <hr />
