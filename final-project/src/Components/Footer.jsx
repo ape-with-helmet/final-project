5722cab71bb13f608ef4e4128a02c1cbc25fd6b8
Sadhguna Aithal <133648612+ape-with-helmet@users.noreply.github.com>
2023-10-31 08:48:59 +0000

    ss

--- final-project/src/Components/Footer.jsx
+++ final-project/src/Components/Footer.jsx
@@ -8,9 +8,7 @@ const Footer = () => {
         <footer class="py-3 my-4">  
           <ul class="nav justify-content-center border-bottom pb-3 mb-3">
             <li class="nav-item"><a href='./' class="nav-link px-2 text-body-light tc">Home</a></li>
-            <li class="nav-item"><a href='./' class="nav-link px-2 text-body-light tc">Features</a></li>
-            <li class="nav-item"><a href='./' class="nav-link px-2 text-body-light tc">Pricing</a></li>
-            <li class="nav-item"><a href='./CP' class="nav-link px-2 text-body-light tc">FAQs</a></li>
+            <li class="nav-item"><a href='./CP' class="nav-link px-2 text-body-light tc">Contact</a></li>
             <li class="nav-item"><a href='./about' class="nav-link px-2 text-body-light tc">About</a></li>
           </ul>
           <p class="text-center text-body-light tc">© 2023 Xerxes Ltd.</p>
