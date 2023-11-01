c6566a4c69712785e97ca2daa2e91e300d6f4be0
Bindu <bindupoojary05@gmail.com>
2023-11-01 10:56:55 +0530

    aboutus

--- final-project/src/Pages/Aboutus.jsx
+++ final-project/src/Pages/Aboutus.jsx
@@ -1,40 +1,27 @@
-// src/components/Header.js
 import React from 'react';
-
-function Header() {
+import './About.css'
+const Aboutus = () => {
   return (
-    <header>
-        <br/>
-      <h1>About Us</h1>
-<h2> Our Story</h2>
-
-<p>Founded in 2003,Xerxes India started as a small family-owned business in the heart of Mangalore. What began as a humble shop with a commitment to delivering top-notch bike accessories has grown into a leading brand trusted by cyclists worldwide.
-
-Over the years, we have evolved, expanded our product line, and embraced cutting-edge technology to ensure that we offer the latest innovations to our customers. However, one thing has remained constant – our dedication to providing the best value and an unparalleled cycling experience.
-</p>
-<h2>Our Mission</h2>
-
-<p>At Xerxes India, our mission is clear: To empower cyclists by providing them with the right tools and accessories they need to ride with confidence, safety, and enjoyment. We believe that every cyclist, from the casual rider to the competitive racer, deserves the best gear to support their passion.</p>
-
-<h2>Why Choose Us?</h2>
-<ol>
-<li>Quality Assurance: We stand by the quality of our products. Every item in our catalog is rigorously tested to meet the highest industry standards.</li>
-
-<li> Customer-Centric:Our commitment to exceptional customer service sets us apart. We're here to assist, educate, and ensure you find the perfect accessories for your biking needs.</li>
-
-<li>Innovation:We keep a close eye on emerging trends and the latest technologies to continually update our product offerings.</li>
-
-<li>Community: We are more than just a brand; we are a community of cyclists who share a love for two wheels. Join us on group rides, and follow us on social media to stay connected.</li>
-
-<li>Sustainability: We are dedicated to reducing our environmental footprint. We strive to offer eco-friendly products and packaging whenever possible.
-</li>
-</ol>
-Thank you for choosing Xerxes India as your trusted source for bike accessories. We look forward to being part of your cycling adventures, and we're excited to support you on your journey, wherever it may lead.
-
-Happy Riding!
-
-    </header>
+    <div className="about">
+    <div className="about-us-container">
+      <h2>About Us</h2>
+      <p>
+      Founded in 2003,Xerxes India started as a small family-owned business in the heart of Mangalore. What began as a 
+      humble shop with a commitment to delivering top-notch bike accessories has grown into a leading brand trusted by 
+      riders worldwide.
+      </p>
+      <p>Over the years, we have evolved, expanded our product line, and embraced cutting-edge technology to ensure that
+         we offer the latest innovations to our customers. However, one thing has remained constant – our dedication to
+          providing the best value and an unparalleled riding experience.
+      </p>
+      <p>We stand by the quality of our products. Every item in our catalog is rigorously tested to meet the highest industry standards.</p>
+      <p>
+      Thank you for choosing Xerxes India as your trusted source for bike accessories. We look forward to being part of
+       your adventures,and we're excited to support you on your journey.
+Happy Riding!!!      </p>
+    </div>
+    </div>
   );
-}
+};
 
-export default Header;
+export default Aboutus;
