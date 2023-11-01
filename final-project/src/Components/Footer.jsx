a2aeab630e3d63b7f40e701dcd8af0b994ed8054
Sadhguna Aithal <ganeshshatrugna@gmail.com>
2023-11-01 21:55:35 +0530

    fixed homepage

--- final-project/src/Components/Footer.jsx
+++ final-project/src/Components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div id="ft">
       <div className="container" >
-        <footer className="py-3 my-4">  
+        <footer className="py-5 my-5 ee">  
           <ul className="nav justify-content-center border-bottom pb-3 mb-3">
             <li className="nav-item"><a href='./' className="nav-link px-2 text-body-light tc">Home</a></li>
             <li className="nav-item"><a href='./CP' className="nav-link px-2 text-body-light tc">Contact</a></li>
