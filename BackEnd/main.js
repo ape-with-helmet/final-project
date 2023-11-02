98e46a61cd7acac4e1330b2160b16b392c28c84c
Sadhguna Aithal <ganeshshatrugna@gmail.com>
2023-11-02 08:52:13 +0530

    d

--- BackEnd/main.js
+++ BackEnd/main.js
@@ -1,77 +1,77 @@
-const express=require("express")
-const cors=require("cors")
+const express = require("express")
+const cors = require("cors")
 const { validEmail, validPassword, validMobile } = require('./src/validations/userValidation');
-const collection=require("./src/models/userModel")
-const contactModel=require("./src/models/contactModel")
+const collection = require("./src/models/userModel")
+const contactModel = require("./src/models/contactModel")
 //const cUser=require("./src/models/cUserModel")
-const app=express()
-const fs=require('fs')
+const app = express()
+const fs = require('fs')
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
-app.get("/",cors(),(req,res)=>{
+app.get("/", cors(), (req, res) => {
 
 })
 
-app.post("/create",async(req,res)=>{
+app.post("/create", async (req, res) => {
     try {
-        const user=req.body
-        const {email,password,mobile}=user;
-        
-        const data={
-            email:email,
-            password:password,
-            mobile:mobile
+        const user = req.body
+        const { email, password, mobile } = user;
+
+        const data = {
+            email: email,
+            password: password,
+            mobile: mobile
         }
-        if (!email || !password || !mobile){
+        if (!email || !password || !mobile) {
             console.log("Please provide all information !")
-            return res.send({message : "Not enough info"})
+            return res.send({ message: "Not enough info" })
         }
         let valEmail = validEmail(email);
         if (!valEmail) {
             console.log("please enter valid email: ")
-            return res.send({message : "invalid id"})
+            return res.send({ message: "invalid id" })
         }
         let valPass = validPassword(password);
         if (!valPass) {
             console.log("please enter valid password: ")
-            return res.send({message : "invalid pass"})
+            return res.send({ message: "invalid pass" })
         }
         let valMob = validMobile(mobile);
         if (!valMob) {
             console.log("please enter valid mobile number: ")
-            return res.send({message : "invalid phone"})
+            return res.send({ message: "invalid phone" })
         }
         //unique validation
-        let uniqueEmail = await collection.findOne({email})
+        let uniqueEmail = await collection.findOne({ email })
         if (uniqueEmail) {
             console.log('mail exists')
-            return res.send({message : "Email already exists!"})
+            return res.send({ message: "Email already exists!" })
         }
-        let uniquePhone = await collection.findOne({mobile})
+        let uniquePhone = await collection.findOne({ mobile })
         if (uniquePhone) {
             console.log('phone exists')
-            return res.send({message : "Phone already exists!"})
+            return res.send({ message: "Phone already exists!" })
         }
         let createUs = await collection.create(data)
         console.log(createUs)
-       // navigate('/login') work needed
+        // navigate('/login') work needed
     } catch (error) {
         console.log(error)
     }
 })
 
-app.post("/delete",async(req,res)=>{
+app.post("/delete", async (req, res) => {
     try {
-        const user=req.body
-        const {email,password}=user;
-        
-        const data={
-            email:email,
-            password:password
+        const user = req.body
+        const { email, password } = user;
+
+        const data = {
+            email: email,
+            password: password
         }
-        let uniqueEmail = await collection.findOne({email})
+        let uniqueEmail = await collection.findOne({ email })
         if (!uniqueEmail) {
             console.log('invalid email')
         }
@@ -81,7 +81,7 @@ app.post("/delete",async(req,res)=>{
     }
 })
 
-app.get("/getall",async(req,res)=>{
+app.get("/getall", async (req, res) => {
     try {
         // const user=req.body
         // const {email,password,/*fname,lname,*/mobile}=user;
@@ -91,24 +91,24 @@ app.get("/getall",async(req,res)=>{
         console.log(error)
     }
 })
-app.post("/login",async(req,res)=>{
+app.post("/login", async (req, res) => {
     try {
-        const user=req.body
-        const {email,password}=user;
+        const user = req.body
+        const { email, password } = user;
         const loginTime = new Date().toISOString();
-        let matchStudent = await collection.findOne({email,password})
+        let matchStudent = await collection.findOne({ email, password })
         console.log(matchStudent)
         if (!matchStudent) {
-            return res.status(500).send({message:"Student not Registered"})//200 is the error code for "Working".
+            return res.status(500).send({ message: "Student not Registered" })//200 is the error code for "Working".
         }
         const logEntry = `${loginTime} - User: ${email}\n`;
         fs.appendFile('log.txt', logEntry, (err) => {
             if (err) {
-            console.error('Error writing to log file:', err);
-            res.status(500).send('Error logging in.');
+                console.error('Error writing to log file:', err);
+                res.status(500).send('Error logging in.');
             } else {
-            console.log('Logged in:', email);
-            res.status(200).send('Logged in successfully.');
+                console.log('Logged in:', email);
+                res.status(200).send('Logged in successfully.');
             }
         });
         console.log('Success login');
@@ -116,7 +116,7 @@ app.post("/login",async(req,res)=>{
         console.log(error)
     }
 });
-app.post("/contactus",async(req,res)=>{
+app.post("/contactus", async (req, res) => {
     try {
         const contact = req.body;
         const { name, email, subject, message } = contact;
@@ -130,12 +130,23 @@ app.post("/contactus",async(req,res)=>{
         }
         let createContact = await contactModel.create(contact);
         res.send({ message: createContact });
-        console.log(createContact)
-        return
-      } catch (err) {
+        const loginTime = new Date().toISOString();
+        const logEntry = `${loginTime}\nUser: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}\n\n`;
+        fs.appendFile('feedback.txt', logEntry, (err) => {
+            if (err) {
+                console.error('Error writing to log file:', err);
+                res.status(500).send('Error logging in.');
+            } else {
+                console.log('Feedback taken:', email);
+                console.log(createContact)
+                return res.status(200)
+            }
+        });
+
+    } catch (err) {
         console.log(err);
-      }
+    }
 });
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log("port connected")
 })
