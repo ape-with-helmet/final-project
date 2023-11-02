d69f40965f626eb4320c60b86bcd1c7a39f90385
Sadhguna Aithal <ganeshshatrugna@gmail.com>
2023-11-02 10:40:41 +0530

    fixed contact

--- final-project/src/Pages/ContactPage.jsx
+++ final-project/src/Pages/ContactPage.jsx
@@ -23,7 +23,7 @@ export default function ContactPage() {
 			})
 			toast.success('Successfully sumbitted', {
 				position: "bottom-center",
-				autoClose: 5000,
+				autoClose: 3000,
 				hideProgressBar: false,
 				closeOnClick: true,
 				pauseOnHover: false,
@@ -35,7 +35,7 @@ export default function ContactPage() {
 			console.log(error)
 			toast.error('error', {
 				position: "bottom-center",
-				autoClose: 5000,
+				autoClose: 3000,
 				hideProgressBar: false,
 				closeOnClick: true,
 				pauseOnHover: false,
