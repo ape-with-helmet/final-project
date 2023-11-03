cbdc009eca0a3c91404dc471f2f5ad06b1743d72
Sadhguna Aithal <ganeshshatrugna@gmail.com>
2023-11-03 21:49:06 +0530

    nav mess

--- final-project/src/Components/NavBar.jsx
+++ final-project/src/Components/NavBar.jsx
@@ -9,12 +9,30 @@ import Search from './Search';
 
 function NavScrollExample() {
   const navigate = useNavigate()
+  const [cartData, setcartData] = useState([]);
   const auth = localStorage.getItem('loginData');
+
   let submit = async (e) => {
     e.preventDefault()
     navigate('/login');
     localStorage.clear();
   }
+
+  useEffect(() => {
+    fetch("http://localhost:8080/getallcart", {
+      method: "GET"
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setcartData(res.data)
+      })
+    console.log(cartData)
+    //const cartLength = cartData.length;
+    console.log(cartData.length)
+    // for (let index = 0; index < cartLength; index++) {
+    //   const cartValue = 
+    // }
+  },[])
   return (
     <>
       <Navbar expand="lg" className="bg-body-tertiary py-0" fixed='top' bg="dark" data-bs-theme="dark">
@@ -32,6 +50,9 @@ function NavScrollExample() {
                   navbarScroll
                 >
                 </Nav>
+                <Button variant="dark-outline" onClick={submit} className='Link keys'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
+                  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                </svg></Button>
                 <Button variant="light" onClick={submit} className='Link keys'>Logout</Button>
               </>
               :
